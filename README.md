d8b6c00997944900e6fc66c7447ad12bbcf0138e
Ombalv <Alpaca222@163.com>
2023-12-02 00:56:49 -0500

    Final Version of Code
    This version should be the final version of our project codes and outputs.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# ECSE-521-Noise-Removal-Project
-get_wav_audio.m is to get wav. format file subsequent processing
-noise_suppression.m is to authentically add stationary white or pink noise to clean speech and use a certain algorithm to remove it.
+# ECSE-512-Noise-Removal-Project
+## Introduction
+This is a course project for McGill ECSE512 Digital Signal Processing. The professor responsible for this course is *Prof. Benoit Champagne*.
+
+### Brief Projection Description
+Reimplement the noise subtraction algorithm by the two papers included in the repo.
+## Files
+get_wav_audio.m is to get wav. format file subsequent processing or you can use your own .wav file either.
+
+noise_suppression.m is to authentically add stationary white or pink noise to clean speech and use a certain algorithm introduced in *Boll*'s paper to remove it.
--- noise_suppression.m
+++ noise_suppression.m
@@ -1,21 +1,58 @@
 clc;
 clear;
 %% Obtain Clean Speech and Add Noise
-[input,Fs] = audioread('clean_speech.wav'); % get clean speech input and the sampling frequency Fs
+clean_speech = './clean speech/novel section.wav';
+[input,Fs] = audioread(clean_speech); % get clean speech input and the sampling frequency Fs
+% resampling to 16000Hz
+[P,Q] = rat(16000/Fs);
+abs(P/Q*Fs-16000);
+Fs = 16000;
+input = resample(input,P,Q);
+
+N = length(input);
+n = 0:N-1;
+w = 2*n*pi/N;
+% clean speech waveform plot
 figure;
-plot(input);
-title('clean speech');
-% sound(input,Fs);
-% pause(10);
+plot(input); 
+xlabel('Samples','FontSize',15);
+ylabel('Amplitude','FontSize',15);
+title('Clean Speech Waveform Plot','FontSize',18);
+
+% clean speech spectrogram
+magnitude_clean = abs(fft(input));
+len = round((length(magnitude_clean))/2);
+figure;
+plot(magnitude_clean(1:len));
+xlabel('Frequency','FontSize',15);
+ylabel('Magnitude','FontSize',15);
+title('Clean Speech Spectrogram','FontSize',18);
+
+
+sound(input,Fs);
+pause(11);
 
 % Add Noise
 snr = 10; %set Signal-Noise-Ratio to 10dB
 noised_signal = add_noise(input, "white noise", snr); % using white noise or pink noise
+% noisy speech waveform plot
 figure;
-plot(noised_signal);
-title('clean speech+noise');
-% sound(noised_signal, Fs);
-% pause(10);
+plot(noised_signal); % noisy speech waveform plot
+xlabel('Samples','FontSize',15);
+ylabel('Amplitude','FontSize',15);
+title('Noisy Speech Waveform Plot','FontSize',18);
+
+% noisy speech spectrogram
+magnitude_noisy = abs(fft(noised_signal));
+len = round((length(magnitude_noisy))/2);
+figure;
+plot(magnitude_noisy(1:len));
+xlabel('Frequency','FontSize',15);
+ylabel('Magnitude','FontSize',15);
+title('Noisy Speech Spectrogram','FontSize',18);
+
+sound(noised_signal, Fs);
+pause(11);
 
 % get the mean of the noise
 timeD_noise = noised_signal - input;
@@ -26,19 +63,56 @@ window = 256;
 noverlap = window/2;
 nfft = window;
 [stft_signal, f_signal, t_signal] = stft(noised_signal,Fs,'Window',hamming(window),'OverlapLength',noverlap,'FFTLength',nfft);
-[stft_noise, f_noise, t_noise] = stft(timeD_noise,Fs,'Window',hamming(window),'OverlapLength',noverlap,'FFTLength',nfft);
-%% Magnitude Averaging and Bias Removal
-magnitude_signal = abs(stft_signal); % get the magnitude of stft_signal
+figure;
+imagesc(t_signal, f_signal, 20*log10((abs(stft_signal(window/2:end,:)))));xlabel('Samples','FontSize',15); ylabel('Freqency','FontSize',15);
+title('Spectrogram after STFT','FontSize',18);
+colormap(gray);
+colorbar;
 
-% Magnitude Averaging
-magnitude_signal = magnitude_avg(magnitude_signal,3);
+% 3D waterfall spectrogram
+% Short-time spectrum of noisy speech
+figure; % figure 6
+waterplot(stft_signal, f_signal, t_signal);
+
+[stft_noise, f_noise, t_noise] = stft(timeD_noise,Fs,'Window',hamming(window),'OverlapLength',noverlap,'FFTLength',nfft);
+%% just for comparison in report
+% without magnitude averaging
+magnitude_signal = abs(stft_signal); % get the magnitude of stft_signal
 stft_noise_mean = mean(abs(stft_noise)); % get [u1,u2,u3,...,uN];
+
 % Get H(e^jw) matrix
-Hejw = 1 - (stft_noise_mean ./ magnitude_signal); 
+Hejw_without_magnitude_averaging = 1 - (stft_noise_mean ./ magnitude_signal);
+
+% Half-Wave Rectification
+Hejw_without_magnitude_averaging = 0.5 .* (Hejw_without_magnitude_averaging + abs(Hejw_without_magnitude_averaging));
+noise_removed_signal_without_magnitude_averaging = Hejw_without_magnitude_averaging .* stft_signal;
+
+% 3D waterfall spectrogram
+% Short-time spectrum using bias removal and half-wave rectification
+figure;% figure 7
+waterplot(noise_removed_signal_without_magnitude_averaging, f_signal, t_signal);
+
+%% Magnitude Averaging and Bias Removal
+% Magnitude Averaging
+mag_avg_num = 3;
+magnitude_signal = magnitude_avg(magnitude_signal,mag_avg_num);
+
+% 3D waterfall spectrogram
+% Short-time spectrum of noisy-speech using three frame averaging
+figure; % figure 8
+waterplot(magnitude_signal, f_signal, t_signal);
+
+% Get H(e^jw) matrix
+Hejw = 1 - (stft_noise_mean ./ magnitude_signal);
 
 % Half-Wave Rectification
 Hejw = 0.5 .* (Hejw + abs(Hejw));
 noise_removed_signal = Hejw .* stft_signal;
+
+% 3D waterfall spectrogram
+% Short-time spectrum using bias removal and half-wave rectification using three frame averaging
+figure; % figure 9
+waterplot(noise_removed_signal, f_signal, t_signal);
 
 % Reduce Noise Residual
 max_N_R = max(abs(stft_noise - stft_noise_mean .* (exp(1i * angle(stft_noise)))));
@@ -47,15 +121,42 @@ noise_removed_signal = res_noise_reduction(noise_removed_signal, max_N_R);
 % Non-speech Activity Signal Attenuation
 avg_noise = stft_noise_mean .* exp(1i * angle(stft_signal));
 noise_removed_signal = non_speech_reduction(noise_removed_signal,avg_noise,stft_signal);
+
+% spectrogram before ISTFT
+figure;
+imagesc(t_signal, f_signal, 20*log10((abs(noise_removed_signal(window/2:end,:)))));xlabel('Samples','FontSize',15); ylabel('Freqency','FontSize',15);
+title('Spectrogram before ISTFT','FontSize',18);
+colormap(gray);
+colorbar;
+
+% 3d waterfall spectrogram
+% Short-time spectrum using bias removal, half-wave rectification, residual noise reduction, and nonspeech signal attenuation
+figure; %figure 11
+waterplot(noise_removed_signal, f_signal, t_signal);
+
 %% ISTFT Function
 [output, Ts] = istft(noise_removed_signal,Fs,'Window',hamming(window),'OverlapLength',noverlap,'FFTLength',nfft);
-figure;
-output = real(output)./abs(real(output)) .* abs(output);
-% output = real(output);
-plot(output);
-title('Noise Removed Signal');
-sound(output,Fs);
 
+% noise removed speech waveform plot
+figure;
+% output = real(output)./abs(real(output)) .* abs(output);
+output = real(output);
+plot(output); % noise removed signal
+xlabel('Samples','FontSize',15);
+ylabel('Amplitude','FontSize',15);
+title('Noise Removed Speech Waveform Plot','FontSize',18);
+
+% noise removed speech spectrogram
+magnitude_noise_removed = abs(fft(output));
+len = round((length(magnitude_noise_removed))/2);
+figure;
+plot(magnitude_noise_removed(1:len));
+xlabel('Frequency','FontSize',15);
+ylabel('Magnitude','FontSize',15);
+title('Noise Removed Speech Spectrogram','FontSize',18);
+sound(output,Fs);
+output_file_name = sprintf('./noise removed speech/output.wav', clean_speech, window, mag_avg_num);
+audiowrite(output_file_name,output,Fs);
 % FUNCTION
 %% Add Noise
 function [output] = add_noise(input,noise_type,snr)
@@ -83,7 +184,7 @@ end
 
 %% Residual Noise Reduction
 function [output] = res_noise_reduction(input, max_N_R)
-assert(size(input, 2) == length(max_N_R), 'max_N_R 大小不匹配');
+assert(size(input, 2) == length(max_N_R), 'max_N_R size not match');
 magnitude_input = abs(input);
 % output = magnitude_input;
 phase_input = angle(input);
@@ -102,10 +203,27 @@ end
 function [output] = non_speech_reduction(input,avg_noise,original_signal)
 output = input;
 T = 20*log10((1/(2*pi))*sum(abs(input ./ avg_noise)));
-disp(T);
 for i=1:size(input,2)
     if T(1,i) < -12
         output(:,i) = 10^(-1.5) .* original_signal(:,i);
     end
 end
+end
+
+function waterplot(s,f,t)
+% Waterfall plot of spectrogram
+    positive_freq_indices = f >= 0;
+    f_positive = f(positive_freq_indices);
+    s_positive = abs(s(positive_freq_indices,:)).^2;
+
+    p = waterfall(f_positive, t, s_positive');
+    % p = waterfall(f,t,abs(s)'.^2);
+    set(gca,XDir="reverse",View=[-35 25]);
+    xlabel("Frequency (Hz)", "FontSize",15);
+    xlim([0, 4e3]);
+    ylabel("Time (s)", "FontSize",15);
+    ylim([0,10]); 
+    zlabel("Amplitude", "FontSize",15);
+    p.EdgeColor = [1,1,1];
+    p.FaceColor = [0,0,0];
 end
